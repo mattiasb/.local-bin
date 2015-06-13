a4e1df8b7d5492e6e99ccfd2975d18b6cb78e929
Mattias Bengtsson <mattias.jc.bengtsson@gmail.com>
2015-06-13 20:18:07 +0200

    Install ncurses-devel

--- init-env.sh
+++ init-env.sh
@@ -191,6 +191,7 @@ function install-packages() {
         mercurial                       \
         npm                             \
         nautilus-open-terminal          \
+        ncurses-devel                   \
         nuntius                         \
         pandoc                          \
         python-pip                      \
